e7cb29ce80d11b920c77b9de38f2d5e878cf239f
Ronnie <1017268540@qq.com>
2019-10-14 15:17:10 +0800

    元编程（十一章）装饰器部分稍微看了一些略有了解，后续深入部分暂且搁置；元类基本了解

--- python_cookbook/capture_11.py
+++ python_cookbook/capture_11.py
@@ -62,7 +62,7 @@ print(add(2, 4))
 
 # 不清楚日志的相关功能如何起效或查看
 #########################################
-class NoInstances(type):
+class NoInstances(type):  # 元类通常继承自type
     def __call__(cls, *args, **kwargs):
         raise TypeError("Can't instantiate directly")
 
@@ -76,6 +76,81 @@ class Spam(metaclass=NoInstances):  # metaclass设置的效果为可以自定义
 
 
 test = Spam.grok(4)
+
+
 # test无法通过test = Spam()进行初始化
 #####################################
+class MyMeta(type):
+    def __init__(cls, clsname, bases, clsdict):
+        for name in clsdict:
+            if 'i' in name:  # 函数名字中有i时会提示
+                print('no character i in name!')
+        return super().__init__(clsname, bases, clsdict)
 
+
+class Root(metaclass=MyMeta):
+    pass
+
+
+class A(Root):
+    def ihere(self):
+        pass
+
+
+class B(Root):
+    def no_wrang(self):
+        pass
+
+
+###########################################################
+
+
+def __init__(self, name, shares, price):  # 定义初始化
+    self.name = name
+    self.shares = shares
+    self.price = price
+
+
+def cost(self):  # 定义函数
+    return self.shares * self.price
+
+
+cls_dict = {
+    '__init__': __init__,
+    'cost': cost,
+}
+
+import types
+
+Stock = types.new_class('Stock', (), {}, lambda ns: ns.update(cls_dict))  # 利用函数建立了类
+Stock.__module__ = __name__  # 定义模块名
+
+s = Stock('ronnie', 12, 1.2)
+print(s.cost())
+
+
+#########################################################################
+def typed_property(name, expected_type):
+    storage_name = '_' + name
+
+    @property
+    def prop(self):
+        return getattr(self, storage_name)
+
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expected_type):
+            raise TypeError('{} must be a {}'.format(name, expected_type))
+        setattr(self, storage_name, value)
+        return prop
+
+
+# Example use
+class Person:
+    name = typed_property('name', str)  # name执行str的类型检查
+    age = typed_property('age', int)  # age执行int的类型检查
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+######################################################
