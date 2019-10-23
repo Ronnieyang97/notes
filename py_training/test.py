1c35d79527eff289b97714146ac98dac3698757b
Ronnie <1017268540@qq.com>
2019-10-23 17:14:17 +0800

    第二十三题 丑数 只能死做

--- py_training/test.py
+++ py_training/test.py
@@ -1,1 +1,32 @@
-print('123'.count('5'))
+from time import time
+
+def get(index):
+    # write code here
+    if index < 1:
+        return 0
+    res = [1]
+    t2 = t3 = t5 = 0
+
+    nextdex = 1
+    while nextdex < index:
+        minNum = min(res[t2] * 2, res[t3] * 3, res[t5] * 5)
+        res.append(minNum)
+
+        # 前进的步伐还是很小的，没一个数都考虑到了
+        while res[t2] * 2 <= minNum:
+            t2 += 1
+        while res[t3] * 3 <= minNum:
+            t3 += 1
+        while res[t5] * 5 <= minNum:
+            t5 += 1
+
+        nextdex += 1
+
+    print(res[nextdex - 1])
+
+
+start = time()
+get(999999)
+end = time()
+
+print(end-start)
