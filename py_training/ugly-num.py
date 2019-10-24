26d8a6ff3b733157a34604d9fb3c8f64ced42c9c
Ronnie <1017268540@qq.com>
2019-10-24 10:48:44 +0800

    第二十三题 丑数 参考了答案

--- py_training/ugly-num.py
+++ py_training/ugly-num.py
@@ -1,6 +1,26 @@
 # 丑数
-#
 # 把只包含因子2、3和5的数称作丑数，6，8是，14不是，习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数
 # 只要某个数不能包含其他素数为其因数
 
 
+def get(index):
+    # write code here
+    if index < 1:
+        return 0
+    res = [1]
+    t2 = t3 = t5 = 0
+
+    nextdex = 1
+    while nextdex < index:
+        minNum = min(res[t2] * 2, res[t3] * 3, res[t5] * 5)
+        res.append(minNum)
+        while res[t2] * 2 <= minNum:
+            t2 += 1
+        while res[t3] * 3 <= minNum:
+            t3 += 1
+        while res[t5] * 5 <= minNum:
+            t5 += 1
+
+        nextdex += 1
+
+    print(res[nextdex - 1])
