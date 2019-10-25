1d512128020fa5657f78f504563e1a8e86c99ab1
Ronnie <1017268540@qq.com>
2019-10-25 13:34:37 +0800

    第三十一题

--- py_training/test.py
+++ py_training/test.py
@@ -1,8 +1,5 @@
-print(int(100/200))
-result = 0
-for i in range(1, 14):
-    result += i
-print(result)
-
-print(sum(range(2, 4)))
+test = 'student. a am I'
+x = test.split()
+x.reverse()
+print(x)
 
